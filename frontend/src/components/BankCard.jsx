dc6f53c14d155d7d02107e589585214f7655763f
Meet Limbachiya <meetlimbachiya2005@gmail.com>
2026-07-12 22:31:11 +0530

    fix: resolve chip overlay enlargement by targeting only new-bank-card-bg SVG class

--- frontend/src/components/BankCard.jsx
+++ frontend/src/components/BankCard.jsx
@@ -23,7 +23,7 @@ export default function BankCard({ type, balance, cardNumber, holderName, onClic
     return (
         <div className={`new-bank-card ${type}`} onClick={onClick}>
             {isShoal && (
-                <svg width="100%" height="100%" viewBox="0 0 360 227" xmlns="http://www.w3.org/2000/svg">
+                <svg className="new-bank-card-bg" width="100%" height="100%" viewBox="0 0 360 227" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <linearGradient id="skyShoal" x1="0" y1="0" x2="0" y2="1">
                       <stop offset="0%" stop-color="#CDEFE6"/>
@@ -64,7 +64,7 @@ export default function BankCard({ type, balance, cardNumber, holderName, onClic
             )}
 
             {isCurrent && (
-                <svg width="100%" height="100%" viewBox="0 0 360 227" xmlns="http://www.w3.org/2000/svg">
+                <svg className="new-bank-card-bg" width="100%" height="100%" viewBox="0 0 360 227" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <linearGradient id="skyCurrent" x1="0" y1="0" x2="0" y2="1">
                       <stop offset="0%" stop-color="#6A5AA8"/>
@@ -96,7 +96,7 @@ export default function BankCard({ type, balance, cardNumber, holderName, onClic
             )}
 
             {isAbyss && (
-                <svg width="100%" height="100%" viewBox="0 0 360 227" xmlns="http://www.w3.org/2000/svg">
+                <svg className="new-bank-card-bg" width="100%" height="100%" viewBox="0 0 360 227" xmlns="http://www.w3.org/2000/svg">
                   <defs>
                     <linearGradient id="skyAbyss" x1="0" y1="0" x2="0" y2="1">
                       <stop offset="0%" stop-color="#171208"/>
